6ec36082f247d1cfdc3991fbe90fbadc1219b19f
Christopher <araujochristopher715@gmail.com>
2024-11-05 11:31:26 +0000

    fff
    fff

---
+++ src/main/java/dad/Main.java
@@ -0,0 +1,12 @@
+package dad;
+
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class Main  {
+
+    public static void main(String[] args){
+        Application.launch(VentanaConMemoriaApp.class , args);
+    }
+}
